1f3d89482d0f53bc9f8b1a38a383102b47928db9
Literallie <devnull@nowak-at.net>
2017-01-25 23:34:17 +0100

    Update config page to 2.0.0 layout

--- pages/config.php
+++ pages/config.php
@@ -25,114 +25,108 @@
 auth_reauthenticate( );
 access_ensure_global_level( config_get( 'manage_plugin_threshold' ) );
 
-html_page_top( plugin_lang_get( 'title' ) );
+layout_page_header( plugin_lang_get( 'title' ) );
+layout_page_begin( 'manage_overview_page.php' );
 
-print_manage_menu( );
+print_manage_menu( 'manage_plugin_page.php' );
 
 ?>
 
-<br />
+<div class="col-md-12 col-xs-12">
+  <div class="space-10"></div>
 
-<form action="<?php echo plugin_page( 'config_edit' )?>" method="post">
-<?php echo form_security_field( 'plugin_HipChat_config_edit' ) ?>
-  <table align="center" class="width75" cellspacing="1">
+  <div class="form-container">
+    <form action="<?php echo plugin_page( 'config_edit' )?>" method="post">
+    <?php echo form_security_field( 'plugin_HipChat_config_edit' ) ?>
+    <div class="widget-box widget-color-blue2">
+      <div class="widget-header widget-header-small">
+        <h4 class="widget-title lighter">
+          <i class="ace-icon fa fa-comment"></i>
+          <?php echo plugin_lang_get( 'title' ) . ' : ' . plugin_lang_get( 'config' ) ?>
+        </h4>
+      </div>
+    </div>
 
-    <tr>
-      <td class="form-title" colspan="3">
-        <?php echo plugin_lang_get( 'title' ) . ' : ' . plugin_lang_get( 'config' )?>
-      </td>
-    </tr>
+    <div class="widget-body">
+      <div class="widget-main no-padding">
+        <div class="table-responsive">
+          <table class="table table-bordered table-condensed table-striped">
+            <tr>
+              <th class="category width-40"><?php echo plugin_lang_get( 'token' ) ?></th>
+              <td>
+                <span class="input">
+                   <input type="text" name="token" class="form-control" value="<?php echo plugin_config_get( 'token' ) ?>" />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <th class="category width-40"><?php echo plugin_lang_get( 'bot_name' ) ?></th>
+              <td>
+                <span class="input">
+                   <input type="text" name="bot_name" class="form-control" value="<?php echo plugin_config_get( 'bot_name' ) ?>" />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <th class="category width-40"><?php echo plugin_lang_get( 'notify' ) ?></th>
+              <td>
+                <span class="input">
+                   <input type="text" name="notify" class="form-control" value="<?php echo plugin_config_get( 'notify' ) ?>" />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <th class="category width-40"><?php echo plugin_lang_get( 'color' ) ?></th>
+              <td>
+                <span class="input">
+                   <input type="text" name="color" class="form-control" value="<?php echo plugin_config_get( 'color' ) ?>" />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <th class="category width-40"><?php echo plugin_lang_get( 'default_room' ) ?></th>
+              <td>
+                <span class="input">
+                   <input type="text" name="default_room" class="form-control" value="<?php echo plugin_config_get( 'default_room' ) ?>" />
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <th class="category width-40">
+                <?php echo plugin_lang_get( 'rooms' ) ?><br />
+                <span class="small">
+                  Specifies the mapping between Mantis project names and HipChat rooms, as an array.<br />
+                  However, it must be set using the <a href="adm_config_report.php">Configuration Report</a> screen, via the <strong>plugin_HipChat_rooms</strong> setting.
+                </span>
+              </th>
+              <td>
+                <pre><?php var_export(plugin_config_get( 'rooms' ))?></pre>
+              </td>
+            </tr>
+            <tr>
+              <th class="category width-40">
+                <?php echo plugin_lang_get( 'columns' ) ?><br />
+                <span class="small">
+                  Specifies the bug fields that should be attached to the HipChat notifications.<br />
+                  However, it must be set using the <a href="adm_config_report.php">Configuration Report</a> screen, via the <strong>plugin_HipChat_columns</strong> setting.<br />
+                  <strong>This setting does not have any effect currrently.</strong>
+                </span>
+              </th>
+              <td>
+                <pre><?php var_export(plugin_config_get( 'columns' ))?></pre>
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div> <!-- /.widget-main -->
 
-    <tr <?php echo helper_alternate_class( )?>>
-      <td class="category">
-        <?php echo plugin_lang_get( 'token' )?>
-      </td>
-      <td  colspan="2">
-        <input type="text" name="token" value="<?php echo plugin_config_get( 'token' )?>" />
-      </td>
-    </tr>
-
-    <tr <?php echo helper_alternate_class( )?>>
-      <td class="category">
-        <?php echo plugin_lang_get( 'bot_name' )?>
-      </td>
-      <td  colspan="2">
-        <input type="text" name="bot_name" value="<?php echo plugin_config_get( 'bot_name' )?>" />
-      </td>
-    </tr>
-
-    <tr <?php echo helper_alternate_class( )?>>
-      <td class="category">
-        <?php echo plugin_lang_get( 'notify' )?>
-      </td>
-      <td  colspan="2">
-        <input type="text" name="notify" value="<?php echo plugin_config_get( 'notify' )?>" />
-      </td>
-    </tr>
-
-    <tr <?php echo helper_alternate_class( )?>>
-      <td class="category">
-        <?php echo plugin_lang_get( 'color' )?>
-      </td>
-      <td  colspan="2">
-        <input type="text" name="color" value="<?php echo plugin_config_get( 'color' )?>" />
-      </td>
-    </tr>
-
-    <tr <?php echo helper_alternate_class( )?>>
-      <td class="category">
-        <?php echo plugin_lang_get( 'default_room' )?>
-      </td>
-      <td  colspan="2">
-        <input type="text" name="default_room" value="<?php echo plugin_config_get( 'default_room' )?>" />
-      </td>
-    </tr>
-
-    <tr <?php echo helper_alternate_class( )?>>
-      <td class="category">
-        <?php echo plugin_lang_get( 'rooms' )?>
-      </td>
-      <td  colspan="2">
-        <p>
-          Specifies the mapping between Mantis project names and HipChat rooms.
-        </p>
-        <p>
-          Option name is <strong>plugin_HipChat_rooms</strong> and is an array of 'Mantis project name' => 'HipChat room name'.
-          Array options must be set using the <a href="adm_config_report.php">Configuration Report</a> screen.
-          The current value of this option is:<pre><?php var_export(plugin_config_get( 'rooms' ))?></pre>
-        </p>
-      </td>
-    </tr>
-
-    <tr <?php echo helper_alternate_class( )?>>
-      <td class="category">
-        <?php echo plugin_lang_get( 'columns' )?>
-      </td>
-      <td  colspan="2">
-        <p>
-          Specifies the bug fields that should be attached to the HipChat notifications.
-        </p>
-        <p>
-          Option name is <strong>plugin_HipChat_columns</strong> and is an array of bug column names. 
-          Array options must be set using the <a href="adm_config_report.php">Configuration Report</a> screen.
-          <?php
-            $t_columns = columns_get_all( $t_project_id );
-            $t_all = implode( ', ', $t_columns );
-          ?>
-          Available column names are:<div><textarea name="all_columns" readonly="readonly" cols="80" rows="5"><?php echo $t_all ?></textarea></div>
-          The current value of this option is:<pre><?php var_export(plugin_config_get( 'columns' ))?></pre>
-        </p>
-      </td>
-    </tr>-
-
-    <tr>
-      <td class="center" colspan="3">
-        <input type="submit" class="button" value="<?php echo lang_get( 'change_configuration' )?>" />
-      </td>
-    </tr>
-
-  </table>
-</form>
+      <div class="widget-toolbox padding-8 clearfix">
+        <input type="submit" class="btn btn-primary btn-white btn-round" value="<?php echo lang_get( 'change_configuration' )?>" />
+      </div>
+    </div> <!-- /.widget-body -->
+  </form>
+  </div>
+</div>
 
 <?php
-html_page_bottom();
+layout_page_end();
